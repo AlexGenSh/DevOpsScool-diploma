4e499f3adbce5872327ee0c206f994b0259b43ee
AlexgenSh <alexgen.scherbakov@gmail.com>
2022-05-14 10:42:16 +0300

    Change deploy_ecr_eks.yml

--- IAC/ouutputs.tf
+++ IAC/ouutputs.tf
@@ -1,30 +1,32 @@
 output "region" {
   description = "AWS region"
-  value       = var.region
+  value       = var.aws_region
 }
 
 output "elb" {
   description = "ELB FQDN"
-  value = resource.kubernetes_service.elb.status[0].load_balancer[0].ingress[0].hostname
+  value = aws_internet_gateway.igw.tags_all
 }
 
-output "elb_id" {
-  description = "ELB ID"
-  value = substr(resource.kubernetes_service.elb.status[0].load_balancer[0].ingress[0].hostname, 0, 32)
-}
+#output "elb_id" {
+#  description = "ELB ID"
+#  value = substr(resource.kubernetes_service.elb.status[0].load_balancer[0].ingress[0].hostname, 0, 32)
+#}
 
 output "db_instance_address" {
   description = "RDS FQDN"
-  value = aws_db_instance.database1.address
+  value = aws_db_instance.diploma-rds-dev.address
 }
 
 
-output "cluster_name" {
-  description = "Kubernetes Cluster Name"
-  value       = var.cluster_name
+output "aws_eks_cluster" {
+  description = "ff"
+  value = aws_eks_cluster.diploma-cluster.endpoint
 }
 
 
-output "kubeconfig" {
-  value = local.kubeconfig
+output "aws_eks_node_group" {
+  description = "ff"
+  value = aws_eks_node_group.diploma-eks-node-group.remote_access
 }
+
